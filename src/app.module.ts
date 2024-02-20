cf3274ba12799276ae8c594d4ac1352773d8adaa
kcholombytko <funnygungan@gmail.com>
2024-02-20 14:07:55 +0200

    Create boilerplate files for user and import config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
